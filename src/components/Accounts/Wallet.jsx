eb69620c26c85ffb38be42ff7c0a80428f935168
Menanya <emmiemenz@gmail.com>
2023-01-25 08:04:48 +0100

    visitors pages

--- src/components/Accounts/Wallet.jsx
+++ src/components/Accounts/Wallet.jsx
@@ -4,13 +4,14 @@ const Wallet = () => {
   return (
     <>
     {/* <div>Wallet</div> */}
-    <div className='flex-justify-space'>
-            <div className='flex-2 border p-li'>
+    <div className='wallet-container'>
+    <div className='wallet flex-justify-space'>
+            <div className='flex-2 border-left p-li'>
                 <h3>johnn Doe</h3>
                 <span>Individual</span>
             </div>
-            <div className='flex-3 border p-li'>
-                <small>CAsh Wallet</small>
+            <div className='flex-3 border-right p-li'>
+                <small>Cash Wallet</small>
                 <p>
                 Once your account has been set up, a Cash Wallet will automatically be created for you.
 
@@ -18,6 +19,7 @@ const Wallet = () => {
                 <a>Finish your account setup</a>
 
             </div>
+        </div>
         </div></>
     
   )
