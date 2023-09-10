9b4c5bf027e5fb513d118d0afde88895c80279f7
morris menanya <emmiemenz@gmail.com>
2023-09-10 08:47:48 +0100

    add faqs

---
+++ src/pages/Admin/assets/AssetView.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const AssetView = () => {
+  return (
+    <div>
+        <div>
+        <div><span>Asset Class: </span><span></span></div>
+
+            <div><span>Asset Title: </span><span></span>
+
+            </div>
+            <div><span>Description: </span><span></span></div>
+            <div><span>minimum_investment: </span><span></span></div>
+            <div><span>Term: </span><span></span></div>
+            <div><span>Payment Schedule: </span><span></span></div>
+            <div><span>Overview Description: </span><span></span></div>
+            <div><span>Overview note: </span><span></span></div>
+            <div><span>Share in Excess: </span><span></span></div>
+            <div><span>Annual Flat Expense in %: </span><span>10</span></div>
+
+        </div>
+    </div>
+  )
+}
+
+export default AssetView
