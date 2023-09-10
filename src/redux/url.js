66421277c72b8600631ddb8de611c3de6c61dfdb
morris menanya <emmiemenz@gmail.com>
2023-09-10 19:14:35 +0100

    connect to server

--- src/redux/url.js
+++ src/redux/url.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://localhost:3000/api/v1/"
+// const baseUrl = "http://localhost:3000/api/v1/"
 
-// const baseUrl = "https://edge-finance-api.onrender.com"
+const baseUrl = "https://edge-finance-api.onrender.com"
 export default baseUrl
